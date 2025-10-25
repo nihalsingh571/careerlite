04c55aeada2885824ff096f2078067b5cfb21424
Nihal kumar singh <ravanwithbabhan@gmail.com>
2025-10-26 00:28:07 +0530

    Branding: switch logo context to local static and rename brand to CareerLite in base templates

--- peeldb/context_processors.py
+++ peeldb/context_processors.py
@@ -1,8 +1,20 @@
+from django.templatetags.static import static
+from django.conf import settings
+
+
 def get_pj_icons(request):
-    logos = {
-        "jobopenings": "http://cdn.peeljobs.com/jobopenings1.png",
-        "logo": "https://cdn.peeljobs.com/logo.png",
-        "favicon": "https://cdn.peeljobs.com/favicon.png",
-        "cdn_path": "https://cdn.peeljobs.com/",
+    try:
+        build = request.build_absolute_uri
+    except Exception:  # pragma: no cover - safety for unusual call sites
+        build = lambda p: p
+
+    # Prefer locally served static assets so branding can be customized
+    logo_url = build(static("logo.png"))
+    favicon_url = build(static("img/favicon.png"))
+
+    return {
+        "jobopenings": logo_url,
+        "logo": logo_url,
+        "favicon": favicon_url,
+        "cdn_path": settings.STATIC_URL,
     }
-    return logos
